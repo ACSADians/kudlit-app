72ffdcc808aed1f423f40e64156bec5021881e32
JamTheDev <2004jamvillarosa@gmail.com>
2026-05-07 23:09:04 +0800

    fix: update build script to use HOME directory for Flutter installation

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-git clone https://github.com/flutter/flutter.git -b stable --depth 1 /opt/flutter
-export PATH="$PATH:/opt/flutter/bin"
+git clone https://github.com/flutter/flutter.git -b stable --depth 1 $HOME/flutter
+export PATH="$PATH:$HOME/flutter/bin"
 flutter config --enable-web
 flutter pub get
 flutter build web --release
